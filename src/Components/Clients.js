3ca2d417f954a7d84e56b4e2b1890213122ad348
Mateusz Wozniak <ennobler@gmail.com>
2021-06-15 11:36:26 +0200

    feature(): Responsive Sliders

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -15,14 +15,30 @@ const Clients = () => {
     slidesToScroll: 2,
     responsive: [
       {
-        breakpoint: 10,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
           dots: true
         }
-      },]
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <section id="Clients" className="ftco-section testimony-section bg-light">
--- src/Components/Team.js
+++ src/Components/Team.js
@@ -47,14 +47,30 @@ const Team = () => {
     slidesToScroll: 2,
     responsive: [
       {
-        breakpoint: 10,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
           dots: true
         }
-      },]
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <section id="Team" className="testimony-section bg-light">
