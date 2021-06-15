b87df954deda54a147aced75e9627f343546d0a7
Mateusz Wozniak <ennobler@gmail.com>
2021-06-15 22:44:42 +0200

    feature(): Sample About background

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -3,52 +3,45 @@ import React, { } from 'react';
 // import { Parallax } from 'react-scroll-parallax';
 
 const AboutUs = () => {
+    const maps = {
+        empty: require('../images/mapka1.jpg'),
+        filled: require('../images/mapka2.jpg'),
+        temp: require('../images/bg_1.jpg'),
+        temp2: require('../images/bg_2.jpg'),
+        temp3: require('../images/temp3.jpg'),
+        temp4: require('../images/temp4.jpg')
+    };
     return (
-        <div>
-            {/* Breadcrumbs */}
-            {/* <section data-stellar-background-ratio="0.5" > 
-                <Parallax classNameName="hero-wrap hero-wrap-2" >
-                    <img className="hero-wrap hero-wrap-2" src={AboutBackground} alt="" />
-                </Parallax>
+        <section id="About" className="hero-wrap image-opacity" style={{ backgroundImage: `url("${maps.temp4}")`, opacity:'0.4' }}>
+        {/* <section id="About" className="hero-wrap"> */}
+            <div className="container">
+                {/* <div id="cf">
+                    <img className="bottom" src={maps.m2} style={{ width: '100%'  }} alt="map" />
+                    <img className="top" src={maps.m1} style={{ width: '100%' }} alt="map" />
+                </div> */}
+                {/* <div className="row">
+                    <div className="col-lg-7 order-md-last d-md-flex align-items-stretch">
 
-                <div className="overlay"></div>
-                <div className="container">
-                    <div className="row no-gutters slider-text js-fullheight align-items-end">
-                        <div className="col-md-9 ftco-animate pb-5">
-                            <p className="breadcrumbs"><span className="mr-2"><a href="index.html">Home <i className="fa fa-chevron-right"></i></a></span> <span>About us <i className="fa fa-chevron-right"></i></span></p>
-                            <h1 className="mb-3 bread">About Us</h1>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
-            <section className="ftco-section ftco-no-pb ftco-no-pt">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-7 order-md-last d-md-flex align-items-stretch">
-                            {/* <div className="img w-100 img-2 mr-md-2" style="background-image: url(images/about.jpg);"></div> */}
-                            <div className="img mr-md-2"></div>
-                            {/* <div className="img w-100 img-2 img-overlay ml-md-2 p-4" style="background-image: url(images/about-2.jpg);"> */}
-                            <div className="img  img-overlay ml-md-2 p-4" >
-                                <div className="request-quote py-5">
-                                    <div className="py-2">
-                                        <span className="subheading">Be Part of our Business</span>
-                                        <h3>Request A Quote</h3>
-                                    </div>
-                                    <p>Domeną naszej pracowni są hale przemysłowe, hale magazynowe, centra logistyczne oraz obiekty biurowe wraz z cała towarzysząca infrastrukturą</p>
+                        <div className="img mr-md-2"></div>
+                        <div className="img  img-overlay ml-md-2 p-4" >
+                            <div className="request-quote py-5">
+                                <div className="py-2">
+                                    <span className="subheading">Be Part of our Business</span>
+                                    <h3>Request A Quote</h3>
                                 </div>
+                                <p>Domeną naszej pracowni są hale przemysłowe, hale magazynowe, centra logistyczne oraz obiekty biurowe wraz z cała towarzysząca infrastrukturą</p>
                             </div>
                         </div>
-                        {/* <div className="col-lg-5 ">
+                    </div> */}
+                    {/* <div className="col-lg-5 ">
                             <div className="heading-section pr-md-5">
                                 <p>Domeną naszej pracowni są hale przemysłowe, hale magazynowe, centra logistyczne oraz obiekty biurowe wraz z cała towarzysząca infrastrukturą</p>
                             </div>
                         </div> */}
-                    </div>
-                </div>
-            </section>
+                {/* </div> */}
+            </div>
+        </section>
 
-        </div>
     );
 }
 
