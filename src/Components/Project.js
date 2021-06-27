24d6864d65fa1287157a1dd3c737e05dd6e8b9fc
Mateusz Wozniak <ennobler@gmail.com>
2021-06-27 20:48:46 +0200

    chore(): build fix

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from "react-router-dom";
 import ProjectsData from '../data/ProjectsData';
 import { useTranslation } from 'react-i18next'
