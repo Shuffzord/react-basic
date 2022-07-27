7cdb8a9612adce6eb3c2c98171e3cf0c12865b3e
Mateusz Wozniak <ennobler@gmail.com>
2022-07-27 15:15:13 +0200

    Carreer small style change

--- src/Components/Carreer.js
+++ src/Components/Carreer.js
@@ -16,11 +16,14 @@ const Carreer = () => {
                 <div className="row align-items-top pt-5">
                     <p className=''>{t('career.main')}</p>
                 </div>
-                <div className="row align-items-top pt-2">
-                    <p className=''>{t('career.extra')}</p>
-                </div>
-                <div className="row d-flex contact-info justify-content-end">
-                    <div className="col-md-4">
+
+                <div className="row d-flex contact-info align-items-end">
+                    <div className="col-md-8">
+                        <div className="row align-items-top pt-2">
+                            <p className=''>{t('career.extra')}</p>
+                        </div>
+                    </div>
+                    <div className="col-md-4 ">
                         <div className="row">
                             <div className="text-center col-md-12">
                                 <p className='col-md-12' style={{ fontWeight: 'bold' }}>{t('career.applications')}</p>
